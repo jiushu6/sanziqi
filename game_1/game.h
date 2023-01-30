90f56fde649cfd8a2bb637bcee54b244e32b6ae3
16594 <16594@JIUSHU>
2023-01-30 15:20:36 +0800

    添加项目文件。

---
+++ game_1/game.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+//符号定义
+#define ROW 3
+#define COL 3
+ 
+
+
+//函数的声明
+
+//初始化棋盘
+void InitBoard(char board [ROW][COL],int row,int col);
+
+//打印棋盘的函数
+void DisplayBoard(char board[ROW][COL], int row, int col);
+
+//玩家下棋
+void PlayerMove(char board[ROW][COL], int row, int col);
+
+//电脑下棋
+void ComputerMove(char board[ROW][COL], int row, int col);
+
+//判断游戏输赢是否继续
+char IsWin(char board[ROW][COL], int row, int col);
